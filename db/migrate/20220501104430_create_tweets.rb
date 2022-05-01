f6599e379ad4a3d3bae20b2331f472d6a76ad74b
shokichi0801 <85923227+shokichi0801@users.noreply.github.com>
2022-05-01 21:17:16 +0900

    ツイートの表示機能実装

--- db/migrate/20220501104430_create_tweets.rb
+++ db/migrate/20220501104430_create_tweets.rb
@@ -1,7 +1,11 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-
+      t.string :name
+      t.string :kcal
+      t.text :image
+      t.datetime :day
+      
       t.timestamps
     end
   end
