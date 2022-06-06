42f4e6b5e2ae8e9d21f46e833f81beb96f34d167
shokichi0801 <85923227+shokichi0801@users.noreply.github.com>
2022-06-06 16:22:39 +0900

    カレンダーの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,19 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+document.addEventListener('turbolinks:load', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new Calendar(calendarEl, {
+    plugins: [ dayGridPlugin, interactionPlugin ]
+  });
+
+  calendar.render();
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
